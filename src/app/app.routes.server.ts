acdaba975e2b8b5dc5c47adb1423f3997075a714
Raihan Nismara <raihan@d3labs.io>
2026-06-06 22:30:32 +0700

    chore: add ssr support, fix build route id on ssr

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'news/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'program/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'knowledge/:id',
+    renderMode: RenderMode.Server,
+  },
 ];
